72a90fd759976244bdbf3b6570abda3dc10d4a38
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-31 18:28:15 +0700

    membuat class dengan final

--- Program noOverriding/Program noOverriding.cpp
+++ Program noOverriding/Program noOverriding.cpp
@@ -3,21 +3,8 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan() {
+	virtual void perkenalan() final {
 		cout << "Hallo saya Function dari base class";
 	}
 };
 
-class derivedClass : public baseClass {
-public:
-	void perkenalan() {
-		cout << "Hallo saya Function dari derived Class";
-	}
-};
-
-int main() {
-	derivedClass a;
-	a.perkenalan();
-
-	return 0;
-}
