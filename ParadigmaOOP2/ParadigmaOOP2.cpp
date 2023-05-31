51e354e9524becfa79c05ddaab3e66cac2497d1d
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-31 17:53:41 +0700

    menambahkan class dengan objek budi

--- ParadigmaOOP2/ParadigmaOOP2.cpp
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -32,3 +32,14 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
