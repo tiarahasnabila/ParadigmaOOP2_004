902ca5804de83d53fca14af61463b9dda4dfe410
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-31 18:34:18 +0700

    membuat  class pelajar dengan akses private

--- program multiwaris/program multiwaris.cpp
+++ program multiwaris/program multiwaris.cpp
@@ -28,3 +28,22 @@ public:
 	}
 };
 
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + "dengan Jenis Kelamin"
+			+ jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
