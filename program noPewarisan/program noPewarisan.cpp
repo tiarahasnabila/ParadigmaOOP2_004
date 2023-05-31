e24b2ac05451eaee51919a887c2a6ea1771b5ff6
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-31 18:10:04 +0700

    membuat class baseclass final

--- program noPewarisan/program noPewarisan.cpp
+++ program noPewarisan/program noPewarisan.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class baseClass {
+class baseClass final {
 public:
 	virtual void perkenalan(){
 		cout << "Hallo saya Function dari base class";
 	}
 };
-class derivedClass : public baseClass {
-public:
-	void perkenalan() {
-		cout << "Hallo saya Function dari derived Class";
-	}
-};
-int main() {
-	derivedClass a;
-	a.perkenalan();
-
-	return 0;
-}
